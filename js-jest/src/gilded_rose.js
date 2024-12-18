bddaa83cc98ff16efd9ee1de4d5cdb8aa4d2d535
WongYC-66 <yengchien@hotmal.com>
2024-12-18 16:29:09 +0800

    add OOP class x4

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -6,12 +6,64 @@ class Item {
   }
 }
 
+class Shop {
+  constructor(items = []) {
+    // this.items = items;
+    // this.items = items.map(item => new ShopableItem(item));
+    this.items = items.map(item => ShopableItem.for(item));
+  }
+  updateQuality() {
+    // this.items.forEach(this.updateItemQuality.bind(this))
+    this.items.forEach(item => {
+      item.updateQuality()
+      item.updateSellIn()
+    })
+    return this.items;
+  }
+}
+
 class ShopableItem extends Item {
+
+  static for(item) {
+    // factory
+    // let shopableItem = this.registry.find(registry => registry.canHandle(item.name))
+    let shopableItem = null
+    switch (item.name) {
+      case 'Aged Brie':
+        shopableItem = ShopableAgedBrid
+        break
+      case 'Sulfuras, Hand of Ragnaros':
+        shopableItem = ShopableSulfuras
+        break
+      case 'Backstage passes to a TAFKAL80ETC concert':
+        shopableItem = ShopableBackstagePass
+        break
+      case 'Conjured':
+        shopableItem = ShopableConjured
+        break
+      default:
+        shopableItem = ShopableItem
+        break
+    }
+
+
+    return new shopableItem(item)
+  }
+
   constructor(item) {
     super(item.name, item.sellIn, item.quality)
   }
 
   updateQuality() {
+    if (this.sellIn == 0) {
+      this.quality -= 2
+    } else {
+      this.quality -= 1
+    }
+    this.quality = this.capLimit(this.quality)
+    return 
+
+
     switch (this.name) {
       case 'Aged Brie':
         if (this.sellIn == 0) {
@@ -54,6 +106,7 @@ class ShopableItem extends Item {
         break
     }
   }
+
   updateSellIn() {
     this.sellIn -= 1
   }
@@ -65,70 +118,55 @@ class ShopableItem extends Item {
   }
 }
 
-class Shop {
-  constructor(items = []) {
-    // this.items = items;
-    this.items = items.map(item => new ShopableItem(item));
-  }
+class ShopableAgedBrid extends ShopableItem {
   updateQuality() {
-    // this.items.forEach(this.updateItemQuality.bind(this))
-    this.items.forEach(item => {
-      item.updateQuality()
-      item.updateSellIn()
-    })
-    return this.items;
-  }
-  // updateItemQuality(item) {
-  //   switch (item.name) {
-  //     case 'Aged Brie':
-  //       if (item.sellIn == 0) {
-  //         item.quality += 2
-  //       } else {
-  //         item.quality += 1
-  //       }
-  //       item.quality = this.capLimit(item.quality)
-  //       break
-  //     case 'Sulfuras, Hand of Ragnaros':
-  //       // sulfuras quality don't change
-  //       break
-  //     case 'Backstage passes to a TAFKAL80ETC concert':
-  //       if (item.sellIn == 0) {
-  //         item.quality = 0
-  //       } else if (item.sellIn <= 5) {
-  //         item.quality += 3
-  //       } else if (item.sellIn <= 10) {
-  //         item.quality += 2
-  //       } else {
-  //         item.quality += 1
-  //       }
-  //       item.quality = this.capLimit(item.quality)
-  //       break
-  //     case 'Conjured':
-  //       if (item.sellIn == 0) {
-  //         item.quality -= 4
-  //       } else {
-  //         item.quality -= 2
-  //       }
-  //       item.quality = this.capLimit(item.quality)
-  //       break
-  //     default:
-  //       if (item.sellIn == 0) {
-  //         item.quality -= 2
-  //       } else {
-  //         item.quality -= 1
-  //       }
-  //       item.quality = this.capLimit(item.quality)
-  //       break
-  //   }
-  //   item.sellIn -= 1
-  // }
-
-  capLimit(quality) {     // adjust quality to between 0 - 50
-    quality = Math.max(quality, 0)
-    quality = Math.min(quality, 50)
-    return quality
+    console.log(this)
+    if (this.sellIn == 0) {
+      this.quality += 2
+    } else {
+      this.quality += 1
+    }
+    this.quality = this.capLimit(this.quality)
   }
 }
+
+class ShopableSulfuras extends ShopableItem {
+  updateQuality() {
+    // do nothing
+  }
+}
+
+class ShopableBackstagePass extends ShopableItem {
+  updateQuality() {
+    if (this.sellIn == 0) {
+      this.quality = 0
+    } else if (this.sellIn <= 5) {
+      this.quality += 3
+    } else if (this.sellIn <= 10) {
+      this.quality += 2
+    } else {
+      this.quality += 1
+    }
+    this.quality = this.capLimit(this.quality)
+  }
+}
+
+class ShopableConjured extends ShopableItem {
+  updateQuality() {
+    if (this.sellIn == 0) {
+      this.quality -= 4
+    } else {
+      this.quality -= 2
+    }
+    this.quality = this.capLimit(this.quality)
+  }
+}
+
+ShopableItem.registry = []
+ShopableItem.registry.push(ShopableAgedBrid)
+ShopableItem.registry.push(ShopableSulfuras)
+ShopableItem.registry.push(ShopableBackstagePass)
+ShopableItem.registry.push(ShopableConjured)
 
 module.exports = {
   Item,
