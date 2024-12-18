75465da6041aa02cc4635e33b781d28c8f68cc73
WongYC-66 <yengchien@hotmal.com>
2024-12-18 15:54:14 +0800

    add: new class ShopableItem

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -6,59 +6,122 @@ class Item {
   }
 }
 
-class Shop {
-  constructor(items = []) {
-    this.items = items;
+class ShopableItem extends Item {
+  constructor(item) {
+    super(item.name, item.sellIn, item.quality)
   }
+
   updateQuality() {
-    this.items.forEach(this.updateItemQuality.bind(this))
-    return this.items;
-  }
-  updateItemQuality(item) {
-    // console.log(this)
-    switch (item.name) {
+    switch (this.name) {
       case 'Aged Brie':
-        if (item.sellIn == 0) {
-          item.quality += 2
+        if (this.sellIn == 0) {
+          this.quality += 2
         } else {
-          item.quality += 1
+          this.quality += 1
         }
-        item.quality = this.capLimit(item.quality)
+        this.quality = this.capLimit(this.quality)
         break
       case 'Sulfuras, Hand of Ragnaros':
         // sulfuras quality don't change
         break
       case 'Backstage passes to a TAFKAL80ETC concert':
-        if (item.sellIn == 0) {
-          item.quality = 0
-        } else if (item.sellIn <= 5) {
-          item.quality += 3
-        } else if (item.sellIn <= 10) {
-          item.quality += 2
+        if (this.sellIn == 0) {
+          this.quality = 0
+        } else if (this.sellIn <= 5) {
+          this.quality += 3
+        } else if (this.sellIn <= 10) {
+          this.quality += 2
         } else {
-          item.quality += 1
+          this.quality += 1
         }
-        item.quality = this.capLimit(item.quality)
+        this.quality = this.capLimit(this.quality)
         break
       case 'Conjured':
-        if (item.sellIn == 0) {
-          item.quality -= 4
+        if (this.sellIn == 0) {
+          this.quality -= 4
         } else {
-          item.quality -= 2
+          this.quality -= 2
         }
-        item.quality = this.capLimit(item.quality)
+        this.quality = this.capLimit(this.quality)
         break
       default:
-        if (item.sellIn == 0) {
-          item.quality -= 2
+        if (this.sellIn == 0) {
+          this.quality -= 2
         } else {
-          item.quality -= 1
+          this.quality -= 1
         }
-        item.quality = this.capLimit(item.quality)
+        this.quality = this.capLimit(this.quality)
         break
     }
-    item.sellIn -= 1
   }
+  updateSellIn() {
+    this.sellIn -= 1
+  }
+
+  capLimit(quality) {     // adjust quality to between 0 - 50
+    quality = Math.max(quality, 0)
+    quality = Math.min(quality, 50)
+    return quality
+  }
+}
+
+class Shop {
+  constructor(items = []) {
+    // this.items = items;
+    this.items = items.map(item => new ShopableItem(item));
+  }
+  updateQuality() {
+    // this.items.forEach(this.updateItemQuality.bind(this))
+    this.items.forEach(item => {
+      item.updateQuality()
+      item.updateSellIn()
+    })
+    return this.items;
+  }
+  // updateItemQuality(item) {
+  //   switch (item.name) {
+  //     case 'Aged Brie':
+  //       if (item.sellIn == 0) {
+  //         item.quality += 2
+  //       } else {
+  //         item.quality += 1
+  //       }
+  //       item.quality = this.capLimit(item.quality)
+  //       break
+  //     case 'Sulfuras, Hand of Ragnaros':
+  //       // sulfuras quality don't change
+  //       break
+  //     case 'Backstage passes to a TAFKAL80ETC concert':
+  //       if (item.sellIn == 0) {
+  //         item.quality = 0
+  //       } else if (item.sellIn <= 5) {
+  //         item.quality += 3
+  //       } else if (item.sellIn <= 10) {
+  //         item.quality += 2
+  //       } else {
+  //         item.quality += 1
+  //       }
+  //       item.quality = this.capLimit(item.quality)
+  //       break
+  //     case 'Conjured':
+  //       if (item.sellIn == 0) {
+  //         item.quality -= 4
+  //       } else {
+  //         item.quality -= 2
+  //       }
+  //       item.quality = this.capLimit(item.quality)
+  //       break
+  //     default:
+  //       if (item.sellIn == 0) {
+  //         item.quality -= 2
+  //       } else {
+  //         item.quality -= 1
+  //       }
+  //       item.quality = this.capLimit(item.quality)
+  //       break
+  //   }
+  //   item.sellIn -= 1
+  // }
 
   capLimit(quality) {     // adjust quality to between 0 - 50
     quality = Math.max(quality, 0)
