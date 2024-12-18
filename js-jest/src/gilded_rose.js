b561adb8813c37b59d2a5609a10c2ff9d60949fa
WongYC-66 <yengchien@hotmal.com>
2024-12-18 16:35:11 +0800

    refactor : factory

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -8,12 +8,9 @@ class Item {
 
 class Shop {
   constructor(items = []) {
-    // this.items = items;
-    // this.items = items.map(item => new ShopableItem(item));
     this.items = items.map(item => ShopableItem.for(item));
   }
   updateQuality() {
-    // this.items.forEach(this.updateItemQuality.bind(this))
     this.items.forEach(item => {
       item.updateQuality()
       item.updateSellIn()
@@ -23,29 +20,31 @@ class Shop {
 }
 
 class ShopableItem extends Item {
+  static canHandle() {
+    return true
+  }
 
   static for(item) {
     // factory
-    // let shopableItem = this.registry.find(registry => registry.canHandle(item.name))
-    let shopableItem = null
-    switch (item.name) {
-      case 'Aged Brie':
-        shopableItem = ShopableAgedBrid
-        break
-      case 'Sulfuras, Hand of Ragnaros':
-        shopableItem = ShopableSulfuras
-        break
-      case 'Backstage passes to a TAFKAL80ETC concert':
-        shopableItem = ShopableBackstagePass
-        break
-      case 'Conjured':
-        shopableItem = ShopableConjured
-        break
-      default:
-        shopableItem = ShopableItem
-        break
-    }
-
+    let shopableItem = this.registry.find(registry => registry.canHandle(item.name))  // Open-closed principle
+    // let shopableItem = null
+    // switch (item.name) {
+    //   case 'Aged Brie':
+    //     shopableItem = ShopableAgedBrid
+    //     break
+    //   case 'Sulfuras, Hand of Ragnaros':
+    //     shopableItem = ShopableSulfuras
+    //     break
+    //   case 'Backstage passes to a TAFKAL80ETC concert':
+    //     shopableItem = ShopableBackstagePass
+    //     break
+    //   case 'Conjured':
+    //     shopableItem = ShopableConjured
+    //     break
+    //   default:
+    //     shopableItem = ShopableItem
+    //     break
+    // }
 
     return new shopableItem(item)
   }
@@ -61,50 +60,6 @@ class ShopableItem extends Item {
       this.quality -= 1
     }
     this.quality = this.capLimit(this.quality)
-    return 
-
-
-    switch (this.name) {
-      case 'Aged Brie':
-        if (this.sellIn == 0) {
-          this.quality += 2
-        } else {
-          this.quality += 1
-        }
-        this.quality = this.capLimit(this.quality)
-        break
-      case 'Sulfuras, Hand of Ragnaros':
-        // sulfuras quality don't change
-        break
-      case 'Backstage passes to a TAFKAL80ETC concert':
-        if (this.sellIn == 0) {
-          this.quality = 0
-        } else if (this.sellIn <= 5) {
-          this.quality += 3
-        } else if (this.sellIn <= 10) {
-          this.quality += 2
-        } else {
-          this.quality += 1
-        }
-        this.quality = this.capLimit(this.quality)
-        break
-      case 'Conjured':
-        if (this.sellIn == 0) {
-          this.quality -= 4
-        } else {
-          this.quality -= 2
-        }
-        this.quality = this.capLimit(this.quality)
-        break
-      default:
-        if (this.sellIn == 0) {
-          this.quality -= 2
-        } else {
-          this.quality -= 1
-        }
-        this.quality = this.capLimit(this.quality)
-        break
-    }
   }
 
   updateSellIn() {
@@ -119,8 +74,11 @@ class ShopableItem extends Item {
 }
 
 class ShopableAgedBrid extends ShopableItem {
+  static canHandle(name) {
+    return name == 'Aged Brie'
+  }
+
   updateQuality() {
-    console.log(this)
     if (this.sellIn == 0) {
       this.quality += 2
     } else {
@@ -131,12 +89,18 @@ class ShopableAgedBrid extends ShopableItem {
 }
 
 class ShopableSulfuras extends ShopableItem {
+  static canHandle(name) {
+    return name == 'Sulfuras, Hand of Ragnaros'
+  }
   updateQuality() {
     // do nothing
   }
 }
 
 class ShopableBackstagePass extends ShopableItem {
+  static canHandle(name) {
+    return name == 'Backstage passes to a TAFKAL80ETC concert'
+  }
   updateQuality() {
     if (this.sellIn == 0) {
       this.quality = 0
@@ -152,6 +116,9 @@ class ShopableBackstagePass extends ShopableItem {
 }
 
 class ShopableConjured extends ShopableItem {
+  static canHandle(name) {
+    return name == 'Conjured'
+  }
   updateQuality() {
     if (this.sellIn == 0) {
       this.quality -= 4
@@ -162,11 +129,12 @@ class ShopableConjured extends ShopableItem {
   }
 }
 
-ShopableItem.registry = []
-ShopableItem.registry.push(ShopableAgedBrid)
-ShopableItem.registry.push(ShopableSulfuras)
-ShopableItem.registry.push(ShopableBackstagePass)
-ShopableItem.registry.push(ShopableConjured)
+// SOLID - Open closed principle
+ShopableItem.registry = [ShopableItem]
+ShopableItem.registry.unshift(ShopableAgedBrid)
+ShopableItem.registry.unshift(ShopableSulfuras)
+ShopableItem.registry.unshift(ShopableBackstagePass)
+ShopableItem.registry.unshift(ShopableConjured)
 
 module.exports = {
   Item,
